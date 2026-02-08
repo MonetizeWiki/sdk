f498bf84772af4625abc8dfd4fc2862d4b8a0f5d
Monetize <dirgantx0@gmail.com>
2026-02-08 20:46:34 +0700

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+export default function Hero() {
+  return (
+    <section className="p-10 text-center">
+      <h2 className="text-3xl font-bold mb-4">
+        Create. Own. Monetize. Socialize.
+      </h2>
+      <Button label="Get Started" />
+    </section>
+  );
+}
